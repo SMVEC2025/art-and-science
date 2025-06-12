57289eeb11aa2d0b1c8a3ac2f0e6af20487dd871
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2025-06-12 10:04:20 +0530

    changes

--- src/pages/home/component/Test.jsx
+++ src/pages/home/component/Test.jsx
@@ -1,46 +1,9 @@
-import React from 'react';
-import { PanoramaViewer } from 'react-pannellum-next';
+import React from 'react'
 
-const Test = () => {
-  const hotSpots = [
-    {
-      pitch: 10, // Vertical angle
-      yaw: 20,   // Horizontal angle
-      type: 'info',
-      text: 'This is the main entrance!',
-      cssClass: 'custom-hotspot-info', // Optional: for custom styling
-      // You can add onClick handlers or other custom props here
-      // onClick: () => alert('Welcome!')
-    },
-    {
-      pitch: -5,
-      yaw: -90,
-      type: 'scene', // For linking to another 360 scene (requires tour config)
-      text: 'Go to the living room',
-      sceneId: 'livingRoomScene', // If you have multiple panoramas in a tour
-      html:`<p>hello</p>`
-    },
-    // Add more hotspots as needed
-  ];
-
+function Test() {
   return (
-    <div style={{ width: '100%', height: '500px' }}>
-      {/*
-        The <PanoramaViewer> component is a placeholder.
-        You'll need to replace it with the actual component
-        from the 360 viewer library you choose (e.g., Pannellum, Photo Sphere Viewer).
-        The props (imagePath, hotSpots) might vary slightly based on the library.
-      */}
-      <PanoramaViewer
-        imagePath="/assets/img/360/rotation.jpg" // Replace with the actual path to your 360 image
-        hotSpots={hotSpots}
-        autoLoad={true}
-        showZoomCtrl={true}
-        showFullscreenCtrl={true}
-        // ... other props specific to your chosen 360 viewer library
-      />
-    </div>
-  );
-};
+    <div>Test</div>
+  )
+}
 
-export default Test;
+export default Test
